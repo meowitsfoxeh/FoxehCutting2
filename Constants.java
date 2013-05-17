beb068560c50f18ddb763961bd5946c8ec4d7200
Foxeh <fultonzoe@gmail.com>
2013-05-17 02:05:06 -0700

    KENNEH HALP

--- Constants.java
+++ Constants.java
@@ -1,5 +1,7 @@
 package FoxehCutting2;
 
+import org.powerbot.game.api.wrappers.Tile;
+
 public class Constants {
 
 	public final static int[] BIRDS_NESTS  = {
@@ -7,4 +9,30 @@ public class Constants {
 	};
 
 	public final static int FIRE_ID = 70758;
+	
+	public final static Tile[] SEERS_MAPLE_PATH = { 
+			new Tile(2723, 3501, 0), 
+			new Tile(2718, 3498, 0), 
+			new Tile(2716, 3493, 0), 
+			new Tile(2716, 3489, 0), 
+			new Tile(2719, 3485, 0), 
+			new Tile(2724, 3484, 0), 
+			new Tile(2726, 3489, 0), 
+			new Tile(2725, 3492, 0)
+	};
+	
+	public final static Tile[] DRAYNOR_WILLOW_PATH = {   
+		new Tile(3092, 3243, 0), 
+	};
+	
+	public final static Tile[] SEERS_YEW_PATH = { 
+			new Tile(2718, 3457, 0), 
+			new Tile(2722, 3460, 0), 
+			new Tile(2725, 3464, 0), 
+			new Tile(2726, 3469, 0), 
+			new Tile(2726, 3474, 0), 
+		    new Tile(2726, 3479, 0), 
+		    new Tile(2725, 3484, 0), 
+		    new Tile(2724, 3489, 0)
+	};
 }
