fbb62c2d28fe3b252c552229dd4932fe468472d9
Foxeh <fultonzoe@gmail.com>
2013-05-17 02:26:07 -0700

    FUCK LIFE

--- BankWood.java
+++ BankWood.java
@@ -1,8 +1,11 @@
 package FoxehCutting2;
 
+import org.powerbot.core.script.job.Task;
 import org.powerbot.core.script.job.state.Node;
 import org.powerbot.game.api.methods.tab.Inventory;
 import org.powerbot.game.api.methods.widget.Bank;
+import org.powerbot.game.api.util.Timer;
+
 
 
 public class BankWood extends Node{
@@ -14,9 +17,22 @@ public class BankWood extends Node{
 	@Override
 	public void execute() {
 		if (Bank.open()) {
+			final Timer bank = new Timer (2000);
+			while(bank.isRunning() && !Bank.isOpen()) {
+				Task.sleep (50);
+			}
 			if (Inventory.contains (Main.mytree.getLogID ())){
-				Bank.depositInventory();}
-			Bank.close();
+				Bank.depositInventory(); 
+				final Timer meow = new Timer (2000);
+				while(meow.isRunning() && !Inventory.isFull()) {
+					Task.sleep (50);
+				}
+				Bank.close();
+				final Timer close = new Timer (2000);
+				while(close.isRunning() && Bank.isOpen ()) {
+					Task.sleep (50);
+				}
+			}
 		}
 	}
 }
